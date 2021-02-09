47f9164682732d464c7a26c235bd3781df9df89a
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-02-09 22:26:52 +0000

    first commit

---
+++ specs/park_spec.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+  let park;
+  let dinosaur1;
+  let dinosaur2;
+
+  beforeEach(function () {
+    park = new Park('Jurassic Park', 100);
+    dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50)
+    dinosaur2 = new Dinosaur('velociraptor', 'carnivore', 25)
+  });
+
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park');
+  });
+
+  it('should have a ticket price', function(){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 100);
+  });
+
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinosaurs;
+    assert.deepEqual(actual, []);
+  });
+
+  it('should be able to add a dinosaur to its collection', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    const actual = park.dinosaurs
+    assert.deepEqual(actual, [dinosaur1, dinosaur2])
+  });
+
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.removeDinosaur(dinosaur2);
+    const actual = park.dinosaurs
+    assert.deepEqual(actual, [dinosaur1])
+  });
+
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    const actual = park.biggestAttraction()
+    assert.strictEqual(actual, 't-rex')
+  });
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
